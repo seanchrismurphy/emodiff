8f0dc03fe193ffe1073724cdaff9347bea7cf0c1
seanchrismurphy <seanchrismurphy@gmail.com>
2020-08-31 12:27:09 +1000

    Adding some processing steps

--- R/emodiff.R
+++ R/emodiff.R
@@ -10,11 +10,14 @@
 #' @param emotions A character vector of all relevant emotion variables in dat
 #' @param ... Grouping variable(s). Usually this will just be a person ID. Occasionally, you might have a wave ID and
 #' a person ID, if you want to calculate scores for each individual within each wave.
+#' @param allow_zero_icc Defaults to FALSE, which sets all emotion differentiation measures to NA if the ICCC for an
+#' individual is 0. Recommended.
+#' @param fisher_transform_icc Defaults to TRUE, which fisher-transforms c_ED and c_nonED (the ICC). Recommended.
 #'
 #' @examples
 #' calculate_ed(dat = emo_ex, emotions = c("happy","relaxed","cheerful"), id)
 
-calculate_ed <- function(dat, emotions, ...) {
+calculate_ed <- function(dat, emotions, ..., allow_zero_icc = FALSE, fisher_transform_icc = TRUE) {
 
   center <- function(x){
     x - mean(x, na.rm = TRUE)
@@ -45,11 +48,23 @@ calculate_ed <- function(dat, emotions, ...) {
   c_dat$m_nonED <- c_dat$momentary_squared_sum/c_dat$m_emo_var
   c_dat$m_ED <- c_dat$momentary_squared_sum/c_dat$m_emo_var*-1
 
+
+  if (fisher_transform_icc) {
+    # We can safely transform before we flip to calculate c_ED or doing filtering, because fisher transform is symmetrical and retains 0s.
+    c_dat$c_nonED <- psych::fisherz(c_dat$c_nonED)
+  }
+
   c_dat$c_ED <- c_dat$c_nonED*-1
 
   c_dat <- c_dat %>% group_by(...) %>% mutate(L2_nonED = sum(m_ED, na.rm = TRUE)/(n()-1))
 
   c_dat$L2_ED <- c_dat$L2_nonED*-1
+
+  if (!allow_zero_icc) {
+    # Set all measures of emotion differentiation to NA if the c_ED = 0
+    c_dat <- c_dat %>% mutate(across(c('m_nonED', 'm_ED', 'c_nonED', 'c_ED', 'L2_nonED', 'L2_ED'),
+                                     function(x) ifelse(c_ED == 0, NA, x)))
+  }
 
 
   out <- left_join(dat, select(c_dat, row_id, m_nonED, m_ED, c_nonED, c_ED, L2_nonED, L2_ED)) %>% select(-row_id)
